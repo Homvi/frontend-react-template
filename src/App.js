076bb5ba73297f8452f1d7d6632e92ff703debf6
homvi <honvedoadamkj@gmail.com>
2023-05-09 07:02:12 +0200

    implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-function App() {
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
+import AdminPage from "./pages/AdminPage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserSpecificPage from "./pages/UserSpecificPage";
+
+const App = () => {
   return (
-    <div className="App">
-     Hello App
-    </div>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/admin" element={<AdminPage />} />
+      <Route path="/user" element={<UserSpecificPage />} />
+    </Routes>
   );
-}
+};
 
 export default App;
